6a3a0a7c034983b9b8c64fc395f1c36a0ef16422
Sandra Scheipl <sscheipl.mmt-b2022@fh-salzburg.ac.at>
2023-12-30 08:06:31 +0100

    changed feature pattern

--- public/wp-content/themes/summit-seekers/pattern/features-grid.php
+++ public/wp-content/themes/summit-seekers/pattern/features-grid.php
@@ -1,18 +1,31 @@
 <?php
+
+
 function get_features_grid()
 {
+    $content = '<div class="feature-grid">';
+
     $args = array(
         'category_name' => 'features',
-        'posts_per_page' => 5 // Set the number of posts to display
+        'posts_per_page' => 3
     );
     $features_query = new WP_Query($args);
 
     if ($features_query->have_posts()) {
         while ($features_query->have_posts()) {
             $features_query->the_post();
-            // Display post content, like the_title() and the_excerpt()
+            $content .= '<div class="feature columns-3 wp-block-post-template-is-layout-grid"">';
+            $content .= '<img src="' . get_the_post_thumbnail() . '" alt=" ">';
+            $content .= '<h3>' . get_the_title() . '</h3>';
+            $content .= '<p>' . get_the_excerpt() . '</p>';
+            $content .= '</div>';
         }
     }
     wp_reset_postdata();
+
+    $content .= '</div>';
+    return $content;
 }
+
+
 ?>
