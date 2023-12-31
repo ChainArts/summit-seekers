c6dfa84029328bf83630a8101b4c37203c16f37e
ChainArts <maxroll54@gmail.com>
2023-12-31 18:07:21 +0100

    fixed buttons and file access

---
+++ public/wp-content/db.php
@@ -0,0 +1,1 @@
+/home/mx/uni/cms/cms-project-scheipl-roll/public/wp-content/plugins/query-monitor/wp-content/db.php
--- public/wp-content/themes/summit-seekers/build/index.asset.php
+++ public/wp-content/themes/summit-seekers/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom'), 'version' => 'f72c78e7026240f70f72');
+<?php return array('dependencies' => array('react', 'react-dom'), 'version' => 'd8e8a3e7dd3b93b73e31');
