dac84ae6094112e3b5bae6916a7e07ae200ec9ec
ChainArts <maxroll54@gmail.com>
2024-01-01 23:08:54 +0100

    simplified index

--- public/wp-content/themes/summit-seekers/index.php
+++ public/wp-content/themes/summit-seekers/index.php
@@ -2,19 +2,12 @@
 
 get_header(); ?>
 
-
-
 <?php if (have_posts()) {
   while(have_posts()) {
     the_post(); ?>
     <div>
-      <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
       <?php the_content(); ?>
     </div>
-
-    <!-- example react component -->
-    <div id="render-react-example-here"></div>
-    <!-- end example react component -->
   <?php }
 }
 
