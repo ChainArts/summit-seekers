a318589feb8c49895aa6da7669c1abb04c3f33b8
ChainArts <maxroll54@gmail.com>
2024-01-13 20:39:20 +0100

    scaled down images

--- public/wp-content/themes/summit-seekers/build/index.asset.php
+++ public/wp-content/themes/summit-seekers/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom'), 'version' => '40ce17a0fc9b086c0c3b');
+<?php return array('dependencies' => array('react', 'react-dom'), 'version' => '5beb58fb570df588ed9a');
--- src/components/headerParallax.jsx
+++ src/components/headerParallax.jsx
@@ -1,53 +1,53 @@
 import React, { useEffect, useRef } from "react";
 import { motion, useTransform, useScroll, cubicBezier } from "framer-motion";
 import Lenis from '@studio-freight/lenis';
-import PR_layer_1 from '../assets/par_bg_layer_1._1920x.webp';
-import PR_layer_2 from '../assets/par_bg_layer_2._1920x.webp';
-import PR_layer_3 from '../assets/par_bg_layer_3._1920x.webp';
-import PR_layer_4 from '../assets/par_bg_layer_4._1920x.webp';
-import PR_layer_5 from '../assets/par_bg_layer_5._1920x.webp';
-import PR_layer_6 from '../assets/par_bg_layer_6._1920x.webp';
-import PR_layer_7 from '../assets/par_bg_layer_7._1920x.webp';
+import PR_layer_1 from '../assets/par_bg_layer_1.webp';
+import PR_layer_2 from '../assets/par_bg_layer_2.webp';
+import PR_layer_3 from '../assets/par_bg_layer_3.webp';
+import PR_layer_4 from '../assets/par_bg_layer_4.webp';
+import PR_layer_5 from '../assets/par_bg_layer_5.webp';
+import PR_layer_6 from '../assets/par_bg_layer_6.webp';
+import PR_layer_7 from '../assets/par_bg_layer_7.webp';
 
 const headerParallax = () => {
-  useEffect(() => {
-    const lenis = new Lenis()
-  
-    function raf(time) {
-      lenis.raf(time)
-      requestAnimationFrame(raf)
-    }
-    requestAnimationFrame(raf)
-  }, []);
-  const containerRef = useRef(null);
-  const { scrollYProgress } = useScroll({
-    target: containerRef,
-    offset: ['start center', 'end start']
-  });
-  const yRangeLayer1 = useTransform(scrollYProgress, [0, 1], [200, 0], {ease: cubicBezier(.14,.8,.4,1)});
-  const yRangeLayer2 = useTransform(scrollYProgress, [0, .95], [1500, 0], {ease: cubicBezier(.14,.8,.4, 1)});
-  const yRangeLayer3 = useTransform(scrollYProgress, [0, .85], [3000, 0], {ease: cubicBezier(.14,.8,.4,1)});
-  const yRangeLayer4 = useTransform(scrollYProgress, [0, .75], [4500, 0], {ease: cubicBezier(.14,.8,.4,1)});
-  const yRangeLayer5 = useTransform(scrollYProgress, [0, .7], [6000, 0], {ease: cubicBezier(.14,.8,.4,1)});
-  const yRangeText = useTransform(scrollYProgress, [0, 1], [-400, 700], {ease: cubicBezier(.14,.8,.4,1)});
-  const yRangeLayer6 = useTransform(scrollYProgress, [0, .6], [9000, 0], {ease: cubicBezier(.14,.8,.4,1)});
-  const yRangeLayer7 = useTransform(scrollYProgress, [0, .5], [11500, 0], { ease: cubicBezier(.14, .8, .4, 1) });
-  const yRangeTrans = useTransform(scrollYProgress, [.45, .9], [100, -100], {ease: cubicBezier(.14,.8,.4,1)});
+    useEffect(() => {
+        const lenis = new Lenis()
 
-  return (
+        function raf(time) {
+            lenis.raf(time)
+            requestAnimationFrame(raf)
+        }
+        requestAnimationFrame(raf)
+    }, []);
+    const containerRef = useRef(null);
+    const { scrollYProgress } = useScroll({
+        target: containerRef,
+        offset: ['start center', 'end start']
+    });
+    const yRangeLayer1 = useTransform(scrollYProgress, [0, 1], [200, 0], { ease: cubicBezier(.14, .8, .4, 1) });
+    const yRangeLayer2 = useTransform(scrollYProgress, [0, .95], [1500, 0], { ease: cubicBezier(.14, .8, .4, 1) });
+    const yRangeLayer3 = useTransform(scrollYProgress, [0, .85], [3000, 0], { ease: cubicBezier(.14, .8, .4, 1) });
+    const yRangeLayer4 = useTransform(scrollYProgress, [0, .75], [4500, 0], { ease: cubicBezier(.14, .8, .4, 1) });
+    const yRangeLayer5 = useTransform(scrollYProgress, [0, .7], [6000, 0], { ease: cubicBezier(.14, .8, .4, 1) });
+    const yRangeText = useTransform(scrollYProgress, [0, 1], [-400, 700], { ease: cubicBezier(.14, .8, .4, 1) });
+    const yRangeLayer6 = useTransform(scrollYProgress, [0, .6], [9000, 0], { ease: cubicBezier(.14, .8, .4, 1) });
+    const yRangeLayer7 = useTransform(scrollYProgress, [0, .5], [11500, 0], { ease: cubicBezier(.14, .8, .4, 1) });
+    const yRangeTrans = useTransform(scrollYProgress, [.45, .9], [100, -100], { ease: cubicBezier(.14, .8, .4, 1) });
 
-    <motion.div ref={containerRef} className="headerParallax">
-      <motion.img src={PR_layer_1} className="parallax-layer" style={{ y: yRangeLayer1, zIndex: 1 }} />
-        <motion.img src={PR_layer_2} className="parallax-layer" style={{ y: yRangeLayer2, zIndex: 5 }} />
-      <motion.img src={PR_layer_3} className="parallax-layer" style={{ y: yRangeLayer3, zIndex: 3 }} />
-      <motion.span style={{ y: yRangeText, zIndex: 4 }} className="hero-text">Summit Seekers</motion.span>
-      <motion.img src={PR_layer_4} className="parallax-layer" style={{ y: yRangeLayer4, zIndex: 5 }} />
-        <motion.img src={PR_layer_5} className="parallax-layer" style={{ y: yRangeLayer5, zIndex: 6 }} />
-        <motion.img src={PR_layer_6} className="parallax-layer" style={{ y: yRangeLayer6, zIndex: 7 }} />
-        <motion.img src={PR_layer_7} className="parallax-layer" style={{ y: yRangeLayer7, zIndex: 8 }} />
-      <motion.div className="hero-transition-gradient" style={{ y: yRangeTrans }}></motion.div>
-    </motion.div>
-      );
+    return (
+
+        <motion.div ref={containerRef} className="headerParallax">
+            <motion.img src={PR_layer_1} className="parallax-layer" style={{ y: yRangeLayer1, zIndex: 1 }} />
+            <motion.img src={PR_layer_2} className="parallax-layer" style={{ y: yRangeLayer2, zIndex: 5 }} />
+            <motion.img src={PR_layer_3} className="parallax-layer" style={{ y: yRangeLayer3, zIndex: 3 }} />
+            <motion.span style={{ y: yRangeText, zIndex: 4 }} className="hero-text">Summit Seekers</motion.span>
+            <motion.img src={PR_layer_4} className="parallax-layer" style={{ y: yRangeLayer4, zIndex: 5 }} />
+            <motion.img src={PR_layer_5} className="parallax-layer" style={{ y: yRangeLayer5, zIndex: 6 }} />
+            <motion.img src={PR_layer_6} className="parallax-layer" style={{ y: yRangeLayer6, zIndex: 7 }} />
+            <motion.img src={PR_layer_7} className="parallax-layer" style={{ y: yRangeLayer7, zIndex: 8 }} />
+            <motion.div className="hero-transition-gradient" style={{ y: yRangeTrans }}></motion.div>
+        </motion.div>
+    );
 }
 
 export default headerParallax;
