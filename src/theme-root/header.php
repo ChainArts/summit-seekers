c83a4dbea67dacb401c92099de4fd68d61c168e5
ChainArts <maxroll54@gmail.com>
2024-01-20 17:08:06 +0100

    fixed favicon path

--- src/theme-root/header.php
+++ src/theme-root/header.php
@@ -4,14 +4,14 @@
         <meta charset="<?php bloginfo('charset'); ?>">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <link rel="apple-touch-icon" sizes="180x180" href="./favicons/apple-touch-icon.png">
-        <link rel="icon" type="image/png" sizes="32x32" href="./favicons/favicon-32x32.png">
-        <link rel="icon" type="image/png" sizes="16x16" href="./favicons/favicon-16x16.png">
-        <link rel="manifest" href="./favicons/site.webmanifest">
-        <link rel="mask-icon" href="./favicons/safari-pinned-tab.svg" color="#54906d">
-        <link rel="shortcut icon" href="./favicons/favicon.ico">
+        <link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_template_directory_uri(); ?>/favicons/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="<?php echo get_template_directory_uri(); ?>/favicons/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="<?php echo get_template_directory_uri(); ?>/favicons/favicon-16x16.png">
+        <link rel="manifest" href="<?php echo get_template_directory_uri(); ?>/favicons/site.webmanifest">
+        <link rel="mask-icon" href="<?php echo get_template_directory_uri(); ?>/favicons/safari-pinned-tab.svg" color="#54906d">
+        <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/favicons/favicon.ico">
         <meta name="msapplication-TileColor" content="#54906d">
-        <meta name="msapplication-config" content="./favicons/browserconfig.xml">
+        <meta name="msapplication-config" content="<?php echo get_template_directory_uri(); ?>/favicons/browserconfig.xml">
         <meta name="theme-color" content="#54906d">
 
         <?php wp_head(); ?>
